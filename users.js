075c7d350e43daa73d1d686182159f0496c4fea5
miraculous-bog <bogdan9110@gmail.com>
2021-09-19 02:15:02 +0300

    edited system notification of tickets

--- users.js
+++ users.js
@@ -1,10 +1,1 @@
-exports.default = [
-  {
-    id: 357629642,
-    name: "vpdofi",
-    nickName: "asdasdasd",
-    status: true,
-    number: "0933445114",
-    state: [4, 1],
-  },
-];
+exports.default = [];
